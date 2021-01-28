b76e599e06a9f374575e9e47ddad82e33f68d1ff
megham-garg <gargmegham3@gmail.com>
2021-01-28 08:42:02 +0530

    Indentaion

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -14,22 +14,22 @@ const Base = ({
       </nav>
       <div className={className}>{children}</div>
       <footer className="footer bg-custom mt-auto pt-3">
-      <div className="container-fluid text-white py-3">
-        <div className="row">
-          <div className="col-5 text-center">
-            <h4>If you got any queries, reach me out at utensiline@gmail.com</h4>
-          </div>
-          <div className="col-2"></div>
-          <div className="col-5">
-              <div className="row"><button className="btn btn-warning btn-lg">Contact Us</button></div>
-              <div className="row">
-              <span className="text-warning font-weight-bold">
-                  An online utensil store by the manufacturers of Jagadhri, India
-              </span>
-              </div>
+        <div className="container-fluid text-white py-3">
+          <div className="row">
+            <div className="col-5 text-center">
+              <h4>If you got any queries, reach me out at utensiline@gmail.com</h4>
+            </div>
+            <div className="col-2"></div>
+            <div className="col-5">
+                <div className="row"><button className="btn btn-warning btn-lg">Contact Us</button></div>
+                <div className="row">
+                <span className="text-warning font-weight-bold">
+                    An online utensil store by the manufacturers of Jagadhri, India
+                </span>
+                </div>
+            </div>
           </div>
         </div>
-      </div>
       </footer>
     </div>
   );
